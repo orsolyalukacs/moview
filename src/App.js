ce7c7462f61dbdae4d41d5b45f13b876317f7990
Orsolya Lucas <orchie.lucas@gmail.com>
2019-03-18 19:38:54 +0100

    Initial commit with create-react app.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>Moview</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
