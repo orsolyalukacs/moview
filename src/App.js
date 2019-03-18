877d8aed6d2a0f820341de887d4c611ac9faef90
Orsolya Lucas <orchie.lucas@gmail.com>
2019-03-18 23:12:23 +0100

    Add SearchBar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import NavBar from './components/NavBar';
+import SearchAppBar from './components/SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <NavBar />
+        <SearchAppBar />
       </div>
     );
   }
