362ecb5c4faeb682cfcd47adb8be2cfd64777f2f
Orsolya Lucas <orchie.lucas@gmail.com>
2019-03-23 16:33:10 +0100

    Add Wikipedia Api call and buttons linking to the movie's Imdb or Wikipedia page.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,6 +6,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import ButtonPrimary from './Button';
+import 'whatwg-fetch';
 
 const styles = theme => ({
   root: {
@@ -18,7 +19,20 @@ const styles = theme => ({
   },
 });
 
+const wikiQueryTitle = "https://en.wikipedia.org/w/api.php?action=query&titles=";
+const relatedTerm = "/similar?api_key=2ab787a73e407248e50ffd9242bd638f";
+const wikiExp = "&prop=extracts&exintro&explaintext&rvprop=content&origin=*&format=json";
+
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showWikiDetails: false,
+      movieOverview: [],
+      WikiPageID: '',
+      isLoading: false,
+    }
+}
   viewDetails() {
     const {movie} = this.props;
     const url = "https://www.themoviedb.org/movie/" + movie.id;
@@ -28,17 +42,60 @@ class MovieList extends Component {
     );
   };
 
-  viewRelated() {
-    const {movie} = this.props;
-    const discoverUrl = "https://api.themoviedb.org/3/movie/" + movie.id + "/similar?api_key=2ab787a73e407248e50ffd9242bd638f&language=en-US&page=1";
+  openWikiPage() {
+    const {wikiPageId} = this.state;
+    const url = "https://en.wikipedia.org/?curid=" + wikiPageId;
     window.open(
-        discoverUrl,
+        url,
         '_blank'
     );
-  };
+  }
+
+  // Query sent to the Wikipedia API
+  wikiHandler() {
+    const {movie} = this.props;
+    const wikiSearchUrl = wikiQueryTitle + movie.title + wikiExp;
+    this.setState({
+      showWikiDetails: true,
+      isLoading: true,
+    });
+
+    fetch(wikiSearchUrl, {
+      method: 'GET',
+    })
+        .then(response => response.json())
+        .then(json => {
+          this.fetchWikiExtract(json);
+          this.setState({
+            isLoading: false,
+          });
+        })
+        .catch(err => {
+          console.log(err.message);
+          this.setState({
+            isLoading: false,
+          });
+        })
+  }
+
+  // Gets movie extract from Wikipedia API
+  fetchWikiExtract(json){
+    console.log(json);
+    let obj = json.query.pages;
+    this.setState({ wikiPageId: obj[Object.keys(obj)[0]].pageid });
+    this.setState({ movieOverview: obj[Object.keys(obj)[0]].extract });
+    console.log(this.state.movieOverview);
+  }
+
+  // Sends related flag to fetch function
+  relatedHandler() {
+    const {updateHandler, movie} = this.props;
+    updateHandler(movie.id, relatedTerm, 'related' );
+  }
 
   render() {
-    const {classes, movie } = this.props;
+    const { showWikiDetails, movieOverview, isLoading } = this.state;
+    const { classes, movie } = this.props;
     return (
         <List className={classes.root}>
           <ListItem alignItems="flex-start">
@@ -51,26 +108,50 @@ class MovieList extends Component {
                         color="primary"
                         className={classes.block}
                         label={movie.title}
-                        onClickHandler={this.viewDetails.bind(this)}
-                        value="Details">
+                        onClickHandler={this.wikiHandler.bind(this)}
+                        value="Wikipedia extract">
                     </ButtonPrimary>
-                    <Typography component="p"
-                                color="textPrimary">
-                      {movie.overview}
-                    </Typography>
+                    { showWikiDetails ?
+                        <Fragment>
+                          {isLoading ?
+                              <div className="spinner"/>
+                              :
+                              <Typography component="p"
+                                          color="textPrimary">
+                                {movieOverview}
+                              </Typography>
+                          }
+                          <ButtonPrimary
+                              variant="text"
+                              color="secondary"
+                              className={classes.block}
+                              label="Imdb"
+                              onClickHandler={this.viewDetails.bind(this)}
+                              value="Imdb">
+                            </ButtonPrimary>
+                          <ButtonPrimary
+                              variant="text"
+                              color="primary"
+                              className={classes.block}
+                              label="Wikipedia"
+                              onClickHandler={this.openWikiPage.bind(this)}
+                              value="Wikipedia">
+                          </ButtonPrimary>
+                          <ButtonPrimary
+                              variant="text"
+                              color="primary"
+                              className={classes.block}
+                              label="Related"
+                              onClickHandler={this.relatedHandler.bind(this)}
+                              value="Related">
+                          </ButtonPrimary>
+                        </Fragment>
+                     : null }
                     <Typography component="ul" className="movie-data-list" color="textSecondary">
                       <li>original release: {movie.release_date}</li>
                       <li>popularity: {movie.popularity}</li>
                       <li>original language: {movie.original_language}</li>
                     </Typography>
-                    <ButtonPrimary
-                        variant="text"
-                        color="primary"
-                        className={classes.block}
-                        label="Related"
-                        onClickHandler={this.viewRelated.bind(this)}
-                        value="Related">
-                    </ButtonPrimary>
                   </Fragment>
                 }
             />
