f3d9c6bbc74a6f93a3a53a6679c884768678f627
Orsolya Lucas <orchie.lucas@gmail.com>
2019-03-24 09:48:03 +0100

    Add MovieInfo component to open movie details in a modal panel.

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,11 +1,53 @@
-import React, { Fragment, Component } from 'react';
-import Modal from '@material-ui/core/Modal';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import ButtonPrimary from './Button';
+import React, { Component } from 'react';
+import Button from './Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
 
 class MovieInfo extends Component {
+  state = {
+    open: true,
+  };
 
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { movieInfo, movie } = this.props;
+    return (
+        <div id="movie-info-wrapper">
+          <Dialog
+              maxWidth="sm"
+              className="movie-info-modal"
+              aria-labelledby="responsive-dialog-title"
+              open={this.state.open}
+              onClose={this.handleClose}
+          >
+            <DialogTitle id="responsive-dialog-title">
+              {movie.title}
+            </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  {movieInfo}
+                   <br/>
+                   <sub>/from wikipedia/</sub>
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClickHandler={this.handleClose}
+                        color="primary"
+                        label="Close">
+                  Close
+                </Button>
+              </DialogActions>
+          </Dialog>
+        </div>
+    );
+  }
 }
 
-export default MovieInfo
+export default withMobileDialog()(MovieInfo);
