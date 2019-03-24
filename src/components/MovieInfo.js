3d35024c5f4e3dd6fa4ba18a5106e75da7fe6749
Orsolya Lucas <orchie.lucas@gmail.com>
2019-03-24 14:44:52 +0100

    Show Imdb button in the modal.

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Button from './Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import withMobileDialog from '@material-ui/core/withMobileDialog';
+
+const styles = ({
+  dialog: {
+    justifyContent: 'space-around',
+  },
+});
 
 class MovieInfo extends Component {
   state = {
@@ -16,40 +22,64 @@ class MovieInfo extends Component {
     this.setState({ open: false });
   };
 
+  openImdb = () => {
+    const {movie} = this.props;
+    const url = "https://www.themoviedb.org/movie/" + movie.id;
+    window.open(
+        url,
+        '_blank'
+    );
+  };
+
   render() {
-    const { movieInfo, movie, dataFrom } = this.props;
+    const { movieInfo, movie, dataFrom, isLoading, classes } = this.props;
     const release_year = (movie.release_date).slice(0, 4);
 
     return (
         <div id="movie-info-wrapper">
-          <Dialog
-              maxWidth="sm"
-              className="movie-info-modal"
-              aria-labelledby="responsive-dialog-title"
-              open={this.state.open}
-              onClose={this.handleClose}
-          >
-            <DialogTitle id="responsive-dialog-title">
-              {movie.title} ({release_year} film)
-            </DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  {movieInfo}
-                   <br/>
-                   <sub>/from {dataFrom}/</sub>
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button onClickHandler={this.handleClose}
+            <Dialog
+                maxWidth="sm"
+                className="movie-info-modal"
+                aria-labelledby="responsive-dialog-title"
+                open={this.state.open}
+                onClose={this.handleClose}
+            >
+              {isLoading ?
+                <div className="spinner"/>
+                :
+                <Fragment>
+                  <DialogTitle id="responsive-dialog-title">
+                    {movie.title} ({release_year} film)
+                  </DialogTitle>
+                  <DialogContent>
+                      <DialogContentText>
+                        {movieInfo}
+                        <br/>
+                        <sub>/from {dataFrom}/</sub>
+                      </DialogContentText>
+                  </DialogContent>
+                  <DialogActions
+                      className={classes.dialog}>
+                    <Button
+                        variant="text"
+                        color="secondary"
+                        label="Imdb"
+                        onClickHandler={this.openImdb}
+                        value="Imdb">
+                    </Button>
+                    <Button
+                        onClickHandler={this.handleClose}
                         color="primary"
                         label="Close">
-                  Close
-                </Button>
-              </DialogActions>
-          </Dialog>
+                      Close
+                    </Button>
+                  </DialogActions>
+                </Fragment>
+                }
+            </Dialog>
         </div>
     );
   }
 }
 
-export default withMobileDialog()(MovieInfo);
+export default  withStyles(styles)(MovieInfo);
